a60bb5cf87b35703f9931f193b42ec0701f4f012
iZ95 <82003318+iZ95@users.noreply.github.com>
2021-11-29 21:01:23 -0800

    Update prefetcher.C

--- prefetcher.C
+++ prefetcher.C
@@ -1,15 +1,43 @@
-#include "prefetcher.h"
+/*
+ *
+ * File: prefetcher.h
+ * Author: Sat Garcia (sat@cs)
+ * Description: Header file for prefetcher implementation
+ *
+ */
+
+#ifndef PREFETCHER_H
+#define PREFETCHER_H
+
+#include <sys/types.h>
 #include "mem-sim.h"
+#include <set>
+class Prefetcher {
+  private:
+	static const u_int32_t  L1_STEP_VALUE=16;
+	static const u_int32_t  L2_STEP_VALUE=32;
+	static const int MAX_L2_BLOCK_DIST=23; //number of L2 blocks ahead of current address to fetch
+	bool ready;
+	u_int32_t nextReqAddr;
 
-bool Prefetcher::hasRequest(u_int32_t cycle) {
-	return false;
-}
+	u_int32_t blockStartAddr(u_int32_t addr, int size);
+  public:
+	Prefetcher();
 
-Request Prefetcher::getRequest(u_int32_t cycle) {
-	Request req;
-	return req;
-}
+	// should return true if a request is ready for this cycle
+	bool hasRequest(u_int32_t cycle);
 
-void Prefetcher::completeRequest(u_int32_t cycle) { return; }
+	// request a desired address be brought in
+	Request getRequest(u_int32_t cycle);
 
-void Prefetcher::cpuRequest(Request req) { return; }
+	// this function is called whenever the last prefetcher request was successfully sent to the L2
+	void completeRequest(u_int32_t cycle);
+
+	/*
+	 * This function is called whenever the CPU references memory.
+	 * Note that only the addr, pc, load, issuedAt, and HitL1 should be considered valid data
+	 */
+	void cpuRequest(Request req); 
+};
+
+#endif
