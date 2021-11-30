0ab8c02c1c7320f15c64ebf8d08acffcc4cb3341
iZ95 <82003318+iZ95@users.noreply.github.com>
2021-11-29 21:45:34 -0800

    Update prefetcher.C

--- prefetcher.C
+++ prefetcher.C
@@ -1,18 +1,10 @@
-/*
- *
- * File: prefetcher.C
- * Author: Sat Garcia (sat@cs)
- * Description: This simple prefetcher waits until there is a D-cache miss then 
- * requests location (addr + 16), where addr is the address that just missed 
- * in the cache.
- *
- */
-
 #include "prefetcher.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <climits>
+
 int count=0;
+
 Prefetcher::Prefetcher():
     nextReqAddr(),ready()
 {
